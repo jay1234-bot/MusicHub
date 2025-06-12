79c7b177f0638ae284bdcecb6eb076678761a56f
krishan <astrohari09@outlook.com>
2025-06-12 12:49:23 +0530

    Update footer.jsx

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -6,11 +6,9 @@ export default function Footer() {
             {/* <div>
                 <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
             </div> */}
-            <p className="text-sm text-muted-foreground">Built for educational purpose. Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://github.com/r2hu1">r2hu1</a>.</p>
+            <p className="text-sm text-muted-foreground"> Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://t.me/censored_politicss">krishan</a>.</p>
             <div className="flex gap-3 items-center mt-3">
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/r2hu1/musichub">Source Code</Link>
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://rahul.eu.org">Portfolio</Link>
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/r.rah_ul">Instagram</Link>
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://t.me/censored_politicss">Telegram</Link>
             </div>
         </footer>
     )
